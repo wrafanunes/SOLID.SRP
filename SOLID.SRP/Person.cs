574ab54e2c8f59bce2a4ade4246d88fb24669b0d
wrafanunes <wrsnunes@gmail.com>
2022-06-23 12:16:53 -0300

    commit inicial
    - criação do projeto violando o princípio da responsabilidade única
    - o exemplo fere o SRP, a classe Program tem mais de um motivo para ser
      altera
    - se para cada entrada do usuário (primeiro nome e último nome) precisar
      de uma nova funcionalidade - como por exemplo pressionar Enter antes
      de continuar - seria necessário inserir a nova funcionalidade em bloco
      if e também no final da classe antes do ultimo ReadLine

---
+++ SOLID.SRP/Person.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Person
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
---
+++ SOLID.SRP/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bem-vindo");
+
+Person person = new();
+
+Console.WriteLine("Qual seu primeiro nome?");
+person.FirstName = Console.ReadLine()!;
+
+Console.WriteLine("Qual seu último nome?");
+person.LastName = Console.ReadLine()!;
+
+if (string.IsNullOrEmpty(person.FirstName))
+{
+    Console.WriteLine("Você não informou um primeiro nome válido");
+    Console.ReadLine();
+    return;
+}
+
+if (string.IsNullOrEmpty(person.LastName))
+{
+    Console.WriteLine("Você não informou um último nome válido");
+    Console.ReadLine();
+    return;
+}
+
+Console.WriteLine($"Seu nome de usuário é {person.FirstName[..1]}{person.LastName}".ToLower());
+
+Console.ReadLine();
